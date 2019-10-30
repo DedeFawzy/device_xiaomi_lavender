1bdb4e15270b9a62fab4306efba0cf632ab9eec6
DedeFawzy <dede.fawzy@gmail.com>
2019-10-31 05:20:38 +0700

    lavender: include GCam

--- device.mk
+++ device.mk
@@ -154,7 +154,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    Snap
+    GoogleCamera
 
 # Shims
 PRODUCT_PACKAGES += \
