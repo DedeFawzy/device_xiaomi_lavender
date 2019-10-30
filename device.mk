15851f7674ab3246c2702dcc892b22f645acdc04
DedeFawzy <dede.fawzy@gmail.com>
2019-10-31 01:36:54 +0700

    kang doze from https://github.com/xiaomi-sdm660

--- device.mk
+++ device.mk
@@ -189,9 +189,9 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service \
     android.hardware.broadcastradio@1.0-impl
 
-# Device-specific settings
-#PRODUCT_PACKAGES += \
-#    XiaomiParts
+# Doze
+PRODUCT_PACKAGES += \
+   XiaomiDoze
 
 # DRM
 PRODUCT_PACKAGES += \
